e9f72528bbb7a05164ef21290d4cf8e8458a7f6f
VanJay <wangwanjie1993@gmail.com>
2020-03-03 14:46:44 +0800

    published HDServiceKit 0.0.1

---
+++ HDServiceKit/HDServiceKit.h
@@ -0,0 +1,17 @@
+//
+//  HDServiceKit.h
+//  HDServiceKit
+//
+//  Created by VanJay on 2020/2/26.
+//  Copyright © 2020 VanJay. All rights reserved.
+//  This file is generated automatically.
+
+#ifndef HDServiceKit_h
+#define HDServiceKit_h
+
+#import <UIKit/UIKit.h>
+
+/// 版本号
+static NSString * const HDServiceKit_VERSION = @"0.0.1";
+
+#endif /* HDServiceKit_h */
