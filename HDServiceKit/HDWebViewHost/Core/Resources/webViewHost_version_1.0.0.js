c6caec4bfd568c6e972f01f4bb88e1bf84940fd4
VanJay <wangwanjie1993@gmail.com>
2020-03-14 18:23:02 +0800

    更新 js sdk 注释写法

--- HDServiceKit/HDWebViewHost/Core/Resources/webViewHost_version_1.0.0.js
+++ HDServiceKit/HDWebViewHost/Core/Resources/webViewHost_version_1.0.0.js
@@ -1,37 +1,42 @@
 !function (global) {
     global.webViewHost = {
-        // 当前版本号
+        /* 当前版本号 */
         version: '1.0.0',
-        // 回调池
+        /* 回调池 */
         callbackPool: {},
-        // 回调 ID
+        /* 回调 ID */
         actionCallbackKey: 1,
-        // 监听池
+        /* 监听池 */
         reqs: {},
-        // 对象是否是方法
+        /* 对象是否是方法 */
         isFunction: function (p) {
             return typeof p === 'function';
         },
-        // 指令通讯
+        /* 指令通讯 */
         invoke: function (_action, _data, _callback) {
             var fullParam = {
                 action: _action,
                 param: _data
             };
-            // 如果有传回调函数，则挂起到回调池
+            /* 如果有传回调函数，则挂起到回调池 */
             if (this.isFunction(_callback)) {
                 fullParam.callbackKey = 'cbk_' + parseInt((Math.random() * 1000), 10) + '_' + this.actionCallbackKey++;
                 this.callbackPool[fullParam.callbackKey] = _callback;
             }
             this.postMessage(fullParam);
         },
-        // 发送指令
+        /* 发送指令 */
         postMessage: function (fullParam) {
             global.kWHScriptHandlerName &&
                 this.isFunction(global.kWHScriptHandlerName.postMessage) &&
                 global.kWHScriptHandlerName.postMessage(JSON.stringify(fullParam));
         },
-        // 原生回调
+        /**
+         * 原生回调，比如调用上传图像 uploadImage
+         * success result: {  msg: 'uploadImage::ok', code: 0, data: { serverId: 111 } }
+         * cancel result: {  msg: 'uploadImage::cancel', code: 0, data: {  } }
+         * fail result: { msg: ' 参数不能为空等等 ', code: 40001 }
+         */
         __callback: function (_callbackKey, _param) {
             if (this.isFunction(this.callbackPool[_callbackKey])) {
                 this.callbackPool[_callbackKey](_param);
@@ -39,10 +44,10 @@
             }
         },
         on: function (_action, _callback) {
-            // 这里暂时只做了一对一回调，如果有一对多需求，可以修改此处逻辑，将 value 改为序列
+            /* 这里暂时只做了一对一回调，如果有一对多需求，可以修改此处逻辑，将 value 改为序列 */
             this.reqs[String(_action)] = _callback;
         },
-        // 原生端触发监听
+        /* 原生端触发监听 */
         __fire: function (_action, _data) {
             var funcCached = this.reqs[String(_action)];
             this.isFunction(funcCached) && funcCached(_data);
