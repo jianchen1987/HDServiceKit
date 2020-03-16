2b0f6acb8d2c3d89eb9e621a863ce15e7964d5f1
VanJay <wangwanjie1993@gmail.com>
2020-03-16 18:05:42 +0800

    H5 Demo 页面增加测试用例入口

--- Example/HDServiceKit/Classes/Controller/Demo/H5/HDH5ViewController.m
+++ Example/HDServiceKit/Classes/Controller/Demo/H5/HDH5ViewController.m
@@ -54,12 +54,16 @@
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    
+
     self.tableView.frame = (CGRect){0, CGRectGetMaxY(self.hd_navigationBar.frame), CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame) - CGRectGetMaxY(self.hd_navigationBar.frame)};
 }
 
 - (void)initDataSource {
     NSArray *dataSource = [NSArray arrayWithObjects:
+                                       @{ @"name": @"自动生成测试用例并加载",
+                                          @"url": @"testcase",
+                                          @"desc": @"该项会自动获取项目中已经实现的方法，并自动渲染成本地 html 并加载，用于自动测试用例"
+                                       },
                                        @{ @"name": @"加载京东页面，拦截京东 JSBridge 协议",
                                           @"url": @"https://item.m.jd.com/ware/view.action?wareId=5904827&sid=null",
                                           @"desc": @"本用例展示：WebViewHost 不仅提供了内置的 JSBridge 协议，还可以和原有的协议共存。\n 通过继承 HDWebViewHostViewController，重载了 decidePolicy 来实现这一点。保持内聚的同时，也具备一定的灵活性。\nWebViewHost 也可以让 native 主动调用自己实现的h5调用原生的能力。将前后端能力统一，满足特殊场景。\n 操作步骤:\n 点击顶部的立即下载，此时弹出一个 toast，内容是京东 JSBridge 接口传的参数。"
@@ -108,14 +112,28 @@
     // 这里只是展示预渲染，大部分情况只需正常加载
     HDWHViewControllerPreRender *_Nonnull preRender = [HDWHViewControllerPreRender defaultRender];
     if (url.length > 0) {
-        [preRender getWebViewController:LoadJDWebViewViewController.class
-                             preloadURL:url
-                             completion:^(HDWebViewHostViewController *_Nonnull vc) {
-                                 if (![vc.url isEqualToString:url]) {
-                                     vc.url = url;
-                                 }
-                                 [self.navigationController pushViewController:vc animated:YES];
-                             }];
+        if ([url isEqualToString:@"testcase"]) {
+            // 检查是否有文件生成，如果没有则遍历
+            NSString *file = [[DocumentsPath stringByAppendingPathComponent:kWebViewHostDBDir] stringByAppendingPathComponent:kWebViewHostTestCaseFileName];
+            if (![HDFileUtil isFileExistedFilePath:file]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+                [HDWHDebugResponse.new performSelector:@selector(generatorHtml)];
+#pragma clang diagnostic pop
+            }
+            LoadJDWebViewViewController *vc = [LoadJDWebViewViewController new];
+            [vc loadLocalFile:[NSURL fileURLWithPath:file] domain:@"https://www.chaosource.com"];
+            [self.navigationController pushViewController:vc animated:YES];
+        } else {
+            [preRender getWebViewController:LoadJDWebViewViewController.class
+                                 preloadURL:url
+                                 completion:^(HDWebViewHostViewController *_Nonnull vc) {
+                                     if (![vc.url isEqualToString:url]) {
+                                         vc.url = url;
+                                     }
+                                     [self.navigationController pushViewController:vc animated:YES];
+                                 }];
+        }
     } else if (fileName.length > 0) {
         [preRender getRenderedViewController:LoadJDWebViewViewController.class
                                   completion:^(UIViewController *_Nonnull obj) {
