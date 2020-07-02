c661f4a6aec35777b9d4828437644971f1996d78
VanJay <wangwanjie1993@gmail.com>
2020-07-02 17:23:48 +0800

    published HDServiceKit 1.0.9

--- HDServiceKit/HDWebViewHost/Core/HDWebViewHostResponseCode.h
+++ HDServiceKit/HDWebViewHost/Core/HDWebViewHostResponseCode.h
@@ -11,11 +11,12 @@
 #define kCode(code) [NSString stringWithFormat:@"%zd", code]
 
 typedef NS_ENUM(NSInteger, HDWHRespCode) {
-    HDWHRespCodeSuccess = 0,        ///< 成功
-    HDWHRespCodeCommonFailed = -1,  ///< 通用失败
-    HDWHRespCodeIllegalArg = -2,    ///< 参数不合法
-    HDWHRespCodeUserCancel = -3,    ///< 用户取消操作
-    HDWHRespCodeUserRejected = -4,  ///< 用户拒绝授权
+    HDWHRespCodeSuccess = 0,           ///< 成功
+    HDWHRespCodeCommonFailed = -1,     ///< 通用失败
+    HDWHRespCodeIllegalArg = -2,       ///< 参数不合法
+    HDWHRespCodeUserCancel = -3,       ///< 用户取消操作
+    HDWHRespCodeUserRejected = -4,     ///< 用户拒绝授权
+    HDWHRespCodeUserNotSignedIn = -5,  ///< 用户未登录
 };
 
 #endif /* HDWebViewHostResponseCode_h */
