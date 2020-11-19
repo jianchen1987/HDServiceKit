f530c1af44c27b6640b941aceb3bdd4568eb7de4
陈剑 <chenj@chaosource.com>
2020-11-19 13:39:54 +0800

    published HDServiceKit 1.2.6

--- HDServiceKit/HDNetwork/Manger/HDNetworkManager.m
+++ HDServiceKit/HDNetwork/Manger/HDNetworkManager.m
@@ -305,9 +305,9 @@
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
             defaultManager = [AFHTTPSessionManager new];
+            defaultManager.completionQueue = dispatch_queue_create("com.hdnetwork.completionqueue", DISPATCH_QUEUE_CONCURRENT);
         });
         manager = defaultManager;
-        manager.completionQueue = dispatch_queue_create("com.hdnetwork.completionqueue", DISPATCH_QUEUE_CONCURRENT);
     }
     AFHTTPResponseSerializer *customSerializer = request.responseSerializer;
     if (customSerializer) manager.responseSerializer = customSerializer;
