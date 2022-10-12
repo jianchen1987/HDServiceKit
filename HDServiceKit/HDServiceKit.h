c3cf78815c20efd866cbf89a9b4e67649948cfe3
chenj <chenj@kh-super.com>
2022-10-12 19:06:27 +0800

    published HDServiceKit 1.6.19

--- HDServiceKit/HDServiceKit.h
+++ HDServiceKit/HDServiceKit.h
@@ -12,7 +12,7 @@
 #import <UIKit/UIKit.h>
 
 /// 版本号
-static NSString *const HDServiceKit_VERSION = @"1.6.18";
+static NSString *const HDServiceKit_VERSION = @"1.6.19";
 
 #if __has_include("HDWHViewControllerPreRender.h")
 #import "HDWHViewControllerPreRender.h"
